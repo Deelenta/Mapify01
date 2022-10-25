c9aa4168a31dfe3636031e683b8a02648da93e23
deele <deele@LAPTOP-PN2MOJJJ>
2022-10-25 08:53:53 +0200

    Add project files.

---
+++ TestingApp/Program.cs
@@ -0,0 +1,17 @@
+﻿Dictionary<string, string> mydictionary = new Dictionary<string, string>();
+
+mydictionary.Add("Key 1", "Value 1");
+mydictionary.Add("Key 2", "Value 2");
+mydictionary.Add("Key 3", "Value 3");
+
+string value;
+bool hasValue = mydictionary.TryGetValue("Key 3", out value);
+if (hasValue)
+{
+    Console.WriteLine(value);
+}
+else
+{
+    Console.WriteLine("Key not present");
+}
+        
